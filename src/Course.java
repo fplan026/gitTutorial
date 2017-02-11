60fa8004379d2af530694000700b953a75522561
fplan026 <fplan026@gmail.com>
2017-02-11 13:20:31 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Nicki on 2017-02-11.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
